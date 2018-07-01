85c83a549d65afaebc1802ceb8e35f2937e59e3d
Prince David <prince.david@hotmail.com>
2018-07-01 19:37:28 +0530

    Initial Commit

---
+++ Instance name.py
@@ -0,0 +1,21 @@
+import boto3
+import sys
+##
+##ec2 = boto3.resource('ec2')
+##
+##for instance in ec2.instances.all():
+##    print(instance.id,instance.state)
+#---------------------------------------
+
+##import boto3
+##
+##ec2 = boto3.resource('ec2')
+##instance = ec2.create_instances(ImageId='ami-14c5486b',MinCount=1,MaxCount=1,InstanceType='t2.micro')
+##print(instance[0].id)
+
+ec2 = boto3.resource('ec2')
+
+##for instance_id in sys.argv[1:]:
+instance = ec2.Instance('i-0f9b99beeefbb0955')
+response = instance.terminate()
+print(response)
