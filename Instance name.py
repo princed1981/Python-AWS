ef87d33a42dcdffc0ed8e598477782a4a1b772b7
Prince David <prince.david@hotmail.com>
2018-07-01 20:32:15 +0530

    Updated the instance code

--- Instance name.py
+++ Instance name.py
@@ -7,15 +7,15 @@ import sys
 ##    print(instance.id,instance.state)
 #---------------------------------------
 
-##import boto3
-##
-##ec2 = boto3.resource('ec2')
-##instance = ec2.create_instances(ImageId='ami-14c5486b',MinCount=1,MaxCount=1,InstanceType='t2.micro')
-##print(instance[0].id)
+import boto3
 
 ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(ImageId='ami-14c5486b',MinCount=1,MaxCount=1,InstanceType='t2.micro')
+print(instance[0].id)
 
-##for instance_id in sys.argv[1:]:
-instance = ec2.Instance('i-0f9b99beeefbb0955')
-response = instance.terminate()
-print(response)
+##ec2 = boto3.resource('ec2')
+##
+####for instance_id in sys.argv[1:]:
+##instance = ec2.Instance('i-0f9b99beeefbb0955')
+##response = instance.terminate()
+##print(response)
